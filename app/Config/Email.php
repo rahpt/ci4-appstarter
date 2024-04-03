edde94dfc3d4f0b2d04d20d5d0b919382cafa3b6
José Proença <github.rahpt@rah.pt>
2024-04-04 00:17:01 +0100

    Initial commit´

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'admin@admin.com';
+    public string $fromName   = 'Admin';
     public string $recipients = '';
 
     /**
